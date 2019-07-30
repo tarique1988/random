e0fac1a373cbc1eeb6193f6f3534008dafb2decd
sajmtam <sajmtam@gmail.com>
2019-07-30 17:15:45 +0000

    Final commit with javascript-time-ago

--- src/item.js
+++ src/item.js
@@ -1,10 +1,16 @@
 import React from "react";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
 const Item = props => {
+  TimeAgo.addLocale(en);
+  const timeAgo = new TimeAgo("en-Us");
   return (
     <div className="item shadowDepth5" onClick={props.clicked}>
       <h3>{props.todo.todo}</h3>
-      <p className="date-time">{props.todo.createdAt.slice(0, 10)}</p>
+      <p className="date-time">
+        {timeAgo.format(new Date(props.todo.createdAt))}
+      </p>
     </div>
   );
 };
